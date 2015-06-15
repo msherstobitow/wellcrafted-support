204815a29ac11897b2fc8fa8f44b779590ff7e1d
Maks Sherstobitow <maksim.sherstobitow@gmail.com>
2015-06-15 17:36:19 +0300

    Add registry property.

--- development/php/classes/support.php
+++ development/php/classes/support.php
@@ -8,6 +8,9 @@ if ( ! defined( 'WELLCRAFTED_SUPPORT' ) ) {
     define( 'WELLCRAFTED_SUPPORT', 'wellcrafted_support' );
 }
 
+/**
+ * @todo  PHPDoc
+ */
 class Wellcrafted_Support extends Wellcrafted_Plugin  {
 
     use Wellcrafted_Singleton_Trait;
@@ -15,6 +18,8 @@ class Wellcrafted_Support extends Wellcrafted_Plugin  {
     protected $use_styles = true;
     protected $use_scripts = true;
     protected $support_email = 'maksim.sherstobitow@gmail.com';
+
+    public static $registry = null;
 
     public function init() {
         new Wellcrafted_Support_Request_Post_Type();
