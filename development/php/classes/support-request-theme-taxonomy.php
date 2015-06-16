df679280ae855598cec968afbc9c4a1d7ca22baf
Maks Sherstobitow <maksim.sherstobitow@gmail.com>
2015-06-16 18:56:08 +0300

    Add better file direct access handler. Improve PHPDoc.

--- development/php/classes/support-request-theme-taxonomy.php
+++ development/php/classes/support-request-theme-taxonomy.php
@@ -1,18 +1,38 @@
 <?php
 
 if ( ! defined( 'ABSPATH' ) ) {
+    header('HTTP/1.0 403 Forbidden');
     exit;
 }
 
 /**
- * @todo  PHPDoc
+ * Wellcrafted_Support_Request_Theme_Taxonomy class object creates a 'wc_support_request_tag' admin taxonomy for 'wc_support_request' post type.
+ * It uses just for tagging support requests.
+ *
+ * @author  Maksim Sherstobitow <maksim.sherstobitow@gmail.com>
+ * @version 1.0.0
+ * @package Wellcrafted\Support
  */
 class Wellcrafted_Support_Request_Theme_Taxonomy extends Wellcrafted_Admin_Taxonomy {
 
+    /**
+     * @todo  PHPDoc
+     */
     protected $taxonomy = 'wc_support_request_tag';
+
+    /**
+     * @todo  PHPDoc
+     */
     protected $object_type = 'wc_support_request';
+
+    /**
+     * @todo  PHPDoc
+     */
     protected $show_admin_column = true;
 
+    /**
+     * @todo  PHPDoc
+     */
     protected function set_params() {
         $this->name_label = __( 'Tags' );
         $this->singular_name_label = __( 'Tag' );
--- development/php/classes/support.php
+++ development/php/classes/support.php
@@ -1,6 +1,7 @@
 <?php
 
 if ( ! defined( 'ABSPATH' ) ) {
+    header('HTTP/1.0 403 Forbidden');
     exit;
 }
 
@@ -9,18 +10,43 @@ if ( ! defined( 'WELLCRAFTED_SUPPORT' ) ) {
 }
 
 /**
- * @todo  PHPDoc
+ * Wellcrafted_Support class is a plugin class.
+ * It allows user to create support requests and send them to recepients.
+ *
+ * @author  Maksim Sherstobitow <maksim.sherstobitow@gmail.com>
+ * @version 1.0.0
+ * @package Wellcrafted\Support
  */
 class Wellcrafted_Support extends Wellcrafted_Plugin  {
 
+    /**
+     * @todo  PHPDoc
+     */
     use Wellcrafted_Singleton_Trait;
 
+    /**
+     * @todo  PHPDoc
+     */
     protected $use_styles = true;
+
+    /**
+     * @todo  PHPDoc
+     */
     protected $use_scripts = true;
+
+    /**
+     * @todo  PHPDoc
+     */
     protected $support_email = 'maksim.sherstobitow@gmail.com';
 
+    /**
+     * @todo  PHPDoc
+     */
     public static $registry = null;
 
+    /**
+     * @todo  PHPDoc
+     */
     public function init() {
         new Wellcrafted_Support_Request_Post_Type();
         new Wellcrafted_Support_Request_Theme_Taxonomy();
