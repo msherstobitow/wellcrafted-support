5189758d0c267a80f1ce285aa5b0e2828da4928d
Maks Sherstobitow <maksim.sherstobitow@gmail.com>
2015-06-17 12:36:42 +0300

    Build.

--- wellcrafted-support/wellcrafted-support.php
+++ wellcrafted-support/wellcrafted-support.php
@@ -2,12 +2,20 @@
 /**
  * Plugin Name: Wellcrafted Support
  * Plugin URI: http://msherstobitow.com/plugins/wellcraftedcore
- * Description: --
+ * Description: The plugin allows to send detailed support requests to themes/plugins authors.
  * Version: 1.0
  * Author: Maksim Sherstobitow
  * Author URI: http://msherstobitow.com
- * License: GPL2
+ * License: GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */ 
+
+/**
+ * Wellcrafted Support initializer
+ * 
+ * @package Wellcrafted\Support
+ * @version  1.0.0
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
     header('HTTP/1.0 403 Forbidden');
@@ -15,7 +23,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * @todo  PHPDoc
+ * Initilize the plugin on Wellcrafted Core action call.
+ *
+ * @since  1.0.0
  */
 add_action( 'wellcrafted_core_initilized', function() {
     require 'classes/support.php';
