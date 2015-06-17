773126c6e7f4f957bb1953b50d9ac2f33e1f2761
Maks Sherstobitow <maksim.sherstobitow@gmail.com>
2015-06-17 12:48:01 +0300

    Update PHPDoc.

--- development/php/classes/support-request-theme-taxonomy.php
+++ development/php/classes/support-request-theme-taxonomy.php
@@ -19,6 +19,7 @@ class Wellcrafted_Support_Request_Theme_Taxonomy extends Wellcrafted_Admin_Taxon
      * The name of the taxonomy. Name should only contain lowercase letters and the underscore character, and not be more than 32 characters long (database structure restriction). 
      * 
      * @var string
+     * @since  1.0.0
      */
     protected $taxonomy = 'wc_support_request_tag';
 
@@ -26,6 +27,7 @@ class Wellcrafted_Support_Request_Theme_Taxonomy extends Wellcrafted_Admin_Taxon
      * Name of the object type for the taxonomy object. Object-types can be built-in Post Type or any Custom Post Type that may be registered. 
      * 
      * @var string or array
+     * @since  1.0.0
      */
     protected $object_type = 'wc_support_request';
 
@@ -33,11 +35,14 @@ class Wellcrafted_Support_Request_Theme_Taxonomy extends Wellcrafted_Admin_Taxon
      * Whether to allow automatic creation of taxonomy columns on associated post-types table. (Available since 3.5) 
      * 
      * @var boolean
+     * @since  1.0.0
      */
     protected $show_admin_column = true;
 
     /**
      * Allows to set params before normalizing
+     * 
+     * @since  1.0.0
      */
     protected function set_params() {
         $this->name_label = __( 'Tags' );
